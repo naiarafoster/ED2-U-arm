7d631eab5fea0e836b2e26747552cab9907695f9
atutuianu2017 <39415120+atutuianu2017@users.noreply.github.com>
2019-01-31 13:21:19 -0500

    Simple code to read analog signal from MyoWare sensor

---
+++ MyoWare-Read.ino
@@ -0,0 +1,27 @@
+/*
+  ReadAnalogVoltage
+  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
+  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+*/
+int sensorValue = 0;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  
+ sensorValue = analogRead(A0);
+ 
+ 
+ //float voltage = sensorValue * (5.0 / 1023.0);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(100);
+}
